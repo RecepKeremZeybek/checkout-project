126d0d8288c18a35d4686f757aa4b7356180cd35
KeremZeybek <recepkeremzeybek@gmail.com>
2023-01-21 16:38:02 +0300

    - +

--- app.js
+++ app.js
@@ -2,8 +2,31 @@ const taxRate = 0.18;
 const shippingPrice = 15;
 const shippingFreePrice = 300;
 
-window.addEventListener("load", ()=> {
-    localStorage.setItem("taxRate",taxRate)
-    localStorage.setItem("shippingPrice",shippingPrice)
-    localStorage.setItem("shippingFreePrice",shippingFreePrice)
-})
+window.addEventListener("load", () => {
+  localStorage.setItem("taxRate", taxRate);
+  localStorage.setItem("shippingPrice", shippingPrice);
+  localStorage.setItem("shippingFreePrice", shippingFreePrice);
+});
+
+const productsDiv = document.querySelector(".products");
+productsDiv.addEventListener("click", (event) => {
+  if (event.target.className == "fa-solid fa-minus") {
+    if (event.target.parentElement.querySelector(".quantity").innerText > 1) {
+      event.target.parentElement.querySelector(".quantity").innerText--;
+    } else {
+      if (
+        confirm(`
+                ${
+                  event.target.parentElement.querySelector("h2").innerText
+                } will be deleted!!!`)
+      ) {
+        event.target.closest(".product").remove();
+      }
+    }
+  } else if (event.target.className == "fa-solid fa-plus") {
+    event.target.previousElementSibling.innerText++;
+  }else if(event.target.className=="remove-product"){
+    // event.target.parentElement.parentElement.parentElement.remove()
+    event.target.closest(".product").remove()
+  }
+});
