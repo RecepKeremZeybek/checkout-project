53c5d43b753cf88bacba170c364c84822c75ac2f
KeremZeybek <recepkeremzeybek@gmail.com>
2023-01-21 15:39:25 +0300

    load

---
+++ app.js
@@ -0,0 +1,9 @@
+const taxRate = 0.18;
+const shippingPrice = 15;
+const shippingFreePrice = 300;
+
+window.addEventListener("load", ()=> {
+    localStorage.setItem("taxRate",taxRate)
+    localStorage.setItem("shippingPrice",shippingPrice)
+    localStorage.setItem("shippingFreePrice",shippingFreePrice)
+})
